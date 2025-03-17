40c3741459e31a9d8ad731fefdb00a40aef826eb
Enrique Chavarria <20230083@ricaldone.edu.sv>
2025-03-17 15:45:31 -0600

    Avance

---
+++ applb/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import Acordeon from './components/Acordion';
+import Alerts from './components/Alerts';
+import Badge from './components/badge';
+function App() {
+  return (
+<>
+<Acordeon/>
+
+<Alerts/>
+
+<Badge/>
+  </>
+  );
+}
+
+export default App
---
+++ applb/src/components/Badge.js
@@ -0,0 +1,7 @@
+function Badge(){
+    return(
+        <h5>Example heading <span class="badge text-bg-secondary">New</span></h5>
+    );
+}
+
+export default Badge;
