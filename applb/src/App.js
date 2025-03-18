d3b7fc3295ce67900f3209fa9785594b17f43a5b
Enrique Chavarria <20230083@ricaldone.edu.sv>
2025-03-18 08:26:49 -0600

    Avance 2

--- applb/src/App.js
+++ applb/src/App.js
@@ -2,15 +2,30 @@ import logo from './logo.svg';
 import './App.css';
 import Acordeon from './components/Acordion';
 import Alerts from './components/Alerts';
-import Badge from './components/badge';
+import Badge from './components/Badge';
+import Buttons from './components/Buttons';
+import Buttons2 from './components/Buttons2';
+import BigButton from './components/BigButton';
+
 function App() {
   return (
 <>
 <Acordeon/>
-
+<br></br>
+<br></br>
 <Alerts/>
-
+<br></br>
+<br></br>
 <Badge/>
+<br></br>
+<br></br>
+<Buttons/>
+<br></br>
+<br></br>
+<Buttons2/>
+<br></br>
+<br></br>
+<BigButton/>
   </>
   );
 }
---
+++ applb/src/components/BigButton.js
@@ -0,0 +1,10 @@
+function BigButton(){
+    return(
+        <>
+<button type="button" class="btn btn-primary btn-lg">Large button</button>
+<button type="button" class="btn btn-secondary btn-lg">Large button</button>
+        </>
+    );
+}
+
+export default BigButton;
